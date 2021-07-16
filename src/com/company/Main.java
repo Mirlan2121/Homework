095fd06938b538598569a53c0277b28d8b525533
Mirlan2121 <zhumataevmirlan@gmail.com>
2021-07-16 23:25:16 +0600

    Домашный задание 37

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,8 @@
+package com.company;
+
+public class Main {
+}
+
 /**
  * create table sports(
  * id serial primary key,
@@ -28,45 +33,136 @@
  * time_visit timestamp not null,
  * trener integer references treners(id)
  * );
+ * ---------------------------проверка-----------------------------------
+ * insert into sports(sport_name, salary)
+ * values
+ * ('Football', 10000),
+ * ('Basketball', 5000),
+ * ('tennis', 7000),
+ * ('swiming', 4000),
+ * ('regby', 3000),
+ * ('POLO', 8500);
+ * commit;
+ * INSERT INTO treners
+ * (trener_name, trener_sername, type_sport, phone, inn)
+ * values
+ * ('Gremlin', 'Nusferatum', 1, +99600001, 'AN001'),
+ * ('ARES', 'MARS', 2, +99600002, 'AN002'),
+ * ('Aura', 'MELAnoskaya', 3,+99600003, 'AN003'),
+ * ('ZENA', 'QuinWars', 4, +99600004, 'AN004'),
+ * ('Terranus', 'Dart', 5, +99600005, 'AN005'),
+ * ('Enakin', 'Skyvoker', 6, +99600006, 'AN006');
+ * commit;
+ * insert into visiters (visiter_name, visiter_sername, data_birthday,gender)
+ * values
+ * ('Mikki', 'Jumanaliev', '13-01-1996', 'male'),
+ * ('Merrim', 'Solikova', '25-11-2000', 'woman'),
+ * ('Alex', 'Zelvitskye', '26-06-1987', 'male'),
+ * ('Lara', 'Croft', '24-03-1987', 'woman'),
+ * ('Sonic', 'Roker', '24-08-1973','male');
+ * commit;
+ * INSERT INTO public.visits
+ * (visiters, time_visit, trener)
+ * values
+ * (1, '2021-01-01 19:30', 1),
+ * (2, '2021-01-06 05:40', 4),
+ * (3, '2021-01-01 09:00', 3),
+ * (4, '2021-01-25 13:00', 4),
+ * (5, '2021-01-08 14:25', 5);
+ * commit;
+ *
+ * select v.id, v.visiter_name, v.gender, s.sport_name, t.trener_name, times.time_visit
+ * from visits times
+ * join sports s on times.trener = s.id
+ * join treners t on t.id = times.trener
+ * join visiters v on v.id = times.visiters
+ * where s.sport_name = 'regby'
+ * order by times.time_visit asc;
  *
  *
- * insert into sports (sport_name, salary)
- * values ('foodball', 15000), ('basketball', 1000), ('tennis', 500), ('swoming', 10000);
- * select * from sports;
+ * ------------------------- Задача Б ---------------------------------------------
  *
- * insert into treners(trener_name, trener_sername, type_sport, phone, inn)
- * values ('trener1', 'trenerSer1', 1, +99655555555550,0000001),('trener2', 'trenerSer2', 2, +99655555555551,0000002),
- * ('trener3', 'trenerSer3', 3, +99655555555553,0000003),('trener4', 'trenerSer4', 4, +99655555555554,0000004);
- * select * from treners;
+ * create table Univer(
+ * id serial primary key,
+ * univer_name varchar
+ * );
+ * create table Fakultet(
+ * id serial primary key,
+ * fakultet_name varchar unique not null,
+ * univer_fakultet integer references Univer not null
+ * );
+ * create table Stady_group(
+ * id serial primary key,
+ * fakultet_group integer references Fakultet unique not null,
+ * code_group float8 not null unique
+ * );
+ * create table Students(
+ * id serial primary key,
+ * spn_student varchar not null,
+ * students_group integer references Stady_group(id) not null,
+ * date_birthday date not null
+ * );
+ * create table Lessons(
+ * id serial primary key,
+ * lessons varchar not null unique
+ * );
+ * create table Appraisal(
+ * id serial primary key,
+ * student integer references Students(id) not null,
+ * app_students integer not null,
+ * lessons_students integer references Lessons(id) not null
+ * );
+ * -------------------------------проверка-----------------------------
  *
- * insert into visiters (visiter_name, visiter_sername, data_birthday, gender)
- * values ('visiterName1', 'VisiterSerName1', '12-11-1990', 'males'), ('visiterName2', 'VisiterSerName2', '02-01-1999', 'woman');
- * select * from visiters;
+ * INSERT INTO public.univer
+ * (univer_name)
+ * VALUES('Универ 1');
+ * commit;
  *
- * insert into visits (visiters, time_visit, trener)
- * values (1,'12:00',1), (2,'13:39',1);
- * select * from visits ;
+ * INSERT INTO public.fakultet
+ * (fakultet_name, univer_fakultet)
+ * values
+ * ('Матиматики', 1),('Физики', 1),('Информатики', 1),
+ * ('Исекуства', 1),('Механики', 1),('Экономики', 1);
+ * commit;
  *
- */
-
-
-/**
- * 1. Для чего нам нужны Базы Данных?
+ * INSERT INTO public.stady_group
+ * (fakultet_group, code_group)
+ * values
+ * (1, 0),(2, 1),(3, 2),
+ * (4, 3),(5, 4),(6, 5);
+ * commit;
  *
- * 2. Что такое первичный ключ (Primary Key)?
- * Первычный ключ дает доступ и связые другие таблицы по уникалнуму ключу и у каждого таблицы он уникален
+ * INSERT INTO public.students
+ * (spn_student, students_group, date_birthday)
+ * values
+ * ('Alexaner Rojkov', 1, '14-12-1989'),('Sam Kusimbekov', 2, '06-07-1995'),
+ * ('Vladimir Paniklov', 3, '30-10-1992'),('Alexaner Solodyankin', 4, '26-11-1990'),
+ * ('Vladimir Solodyankin', 5, '03-05-1970');
+ * commit;
  *
- * 3. Что такое внешний ключ (Foreign Key)?
- *  Foreign key работает как ссылка к таблицам связыет между собой
+ * INSERT INTO public.lessons
+ * (lessons)
+ * values
+ * ('Матиматека'),('Геометрия'),('Английсткий'),('Русский'),
+ * ('Изо'),('Физика'),('Химия'),('Астраномия'),('Алхимия');
+ * commit;
  *
- * 4. Чем отличаются колонки от строк?
- * сторака строение данных в линню и фа ни делетяс строки где пишится слова, цифрыб т.д .Колонки не делятяс на строки
- * он состоят в определен порялке в один массив с обазначаетс опреденный строку
+ * INSERT INTO public.appraisal
+ * (student, app_students, lessons_students)
+ * values
+ * (3, 3, 1),(3, 5, 2),(3, 4, 3),(3, 1, 4),
+ * (3, 2, 5),(3, 3, 6),(3, 4, 7),(3, 5, 8),
+ * (3, 5, 9),(1, 3, 1),(2, 3, 2);
+ * commit;
  *
- * 5. Чем отличаются таблицы от базы данных?
- * База даннных хранит все что нужно пользователю. Таблица нет у него определенный структура храннение данных.
+ * select AVG(a.app_students) as Avareg_app from appraisal a
+ * join students s on s.id = 3 and s.id = a.student ;
  *
- * 6. Что такое CRUD операции?
- * это операция котороя читает создает изменяет и удоляет. Я так пологаю что это некая операция которая должа выполнять все эти функции и должна выполнять
- * это все фактически автоматически
+ * select s.id, s.spn_student, f.fakultet_name, sg.code_group, u.univer_name
+ * from students s
+ * join stady_group sg on sg.id = s.students_group
+ * join fakultet f on f.id = sg.fakultet_group
+ * join univer u on u.id = f.univer_fakultet
+ * where s.id = 6;
  */
